034fa1716dc865ecce4d6da58eb7a64e519107fc
Ivan Pivnev <pivnev-1998@mail.ru>
2023-02-18 21:30:22 +0300

    Дописал домашку

--- Primeri/Primer_3/Program.cs
+++ Primeri/Primer_3/Program.cs
@@ -3,3 +3,27 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+bool ValidateNum(int number)
+{
+    if (number < 1 || number > 7)
+    {
+        System.Console.WriteLine("Вы ввели неверное число!");
+        return false;
+    }
+    return true;  
+}
+System.Console.WriteLine("Введите число дня недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(ValidateNum(number))
+{
+if (number > 5)
+    {
+        System.Console.WriteLine("Этот день выходной!");
+    }
+else
+    {
+        System.Console.WriteLine("Это рабочий день!");
+    }
+
+}
